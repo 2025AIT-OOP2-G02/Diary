fe0df18c00df34c3671f78b18e3434dbf4400152
naka2005 <103171123+naka2005@users.noreply.github.com>
2025-10-16 15:43:01 +0900

    main

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from diaries.DiarySample import DiarySample
 from diaries.nakamotoDiary import nakamotoDiary
-
+from diaries.NakayamaDiary import NakayamaDiary
+from diaries.ODiary import DiarySample as DS
+from diaries.K24012_Diary import K24012_Diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
     nakamotoDiary(),
+    NakayamaDiary(),
+    DS(),
+    K24012_Diary(),
 ]
-for d in diaries:   
+for d in diaries:
     print("---------------------------------")
     print(d.get_date())
     print(d.get_summary())
