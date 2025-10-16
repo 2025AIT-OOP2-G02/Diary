03552e189d2225a550bc28b3a721f90dad84f523
tubu-Git <shouki_0118@au.com>
2025-10-16 15:29:57 +0900

    k24012_diary

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from diaries.DiarySample import DiarySample
+from diaries.K24012_Diary import K24012_Diary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
-    DiarySample(),
+    K24012_Diary(),
 ]
 for d in diaries:
     print("---------------------------------")
