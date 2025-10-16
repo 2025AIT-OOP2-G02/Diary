f98b15c5ff28e5525e8817bd767ff59d7b4232cd
nakamoto007 <109nanakaka@gmail.com>
2025-10-16 15:07:02 +0900

    nakamoto add

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from diaries.DiarySample import DiarySample
+from diaries.nakamotoDiary import nakamotoDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
     DiarySample(),
+    nakamotoDiary(),
 ]
-for d in diaries:
+for d in diaries:   
     print("---------------------------------")
     print(d.get_date())
     print(d.get_summary())
