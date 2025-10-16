6d1284a3aded1389fdc7ce36cedc49440ec321db
tubu-Git <shouki_0118@au.com>
2025-10-16 15:09:30 +0900

    Update DiarySample.py

--- diaries/DiarySample.py
+++ diaries/DiarySample.py
@@ -3,10 +3,10 @@ from diaries.AbstractDiary import AbstractDiary
 
 class DiarySample(AbstractDiary):
     def get_date(self):
-        return "2021-12-01"
+        return "2025-10-16"
 
     def get_summary(self):
         return "なにもない一日だった"
 
     def get_author(self):
-        return "Sample"
+        return "k24012"
